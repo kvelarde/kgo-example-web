2256fbaa39fdcfded656c76a31937166afdbb95b
Kurtis Velarde <kvelarde@spokeo.com>
2017-02-27 21:01:54 -0800

    Adding webapp

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	port := os.Getenv("LISTEN_PORT")
+	message := os.Getenv("LISTEN_MSG")
+
+	if port == "" {
+		log.Printf("No Listen port specifed defulting to 45678")
+		port = "45678"
+	}
+
+	if message == "" {
+		log.Printf("No message, defaulting to hello")
+		message = "hello!"
+	}
+
+	if _, err := strconv.Atoi(port); err != nil {
+		log.Fatalf("Invalid port: %s (%s)\n", port, err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		println("---->", port, r.URL.String())
+		res := bytes.NewBufferString("This is App " + message + "\n\n")
+		w.WriteHeader(200)
+		w.Write(res.Bytes())
+	})
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
