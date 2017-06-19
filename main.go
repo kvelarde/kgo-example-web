344db20edfe35f67ff0b4db746d663b04b697166
kurtis velarde <kurtisvelarde@gmail.com>
2017-06-19 23:02:35 +0000

    updating msg

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 
 	if message == "" {
 		log.Printf("No message, default: hello")
-		message = "hello Dan!!"
+		message = "hello kevin!!"
 	}
 
 	if _, err := strconv.Atoi(port); err != nil {
