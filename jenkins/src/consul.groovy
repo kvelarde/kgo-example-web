f945f3ca0ad8865f65517cece29a3dba1a90f6e3
Kurtis Velarde <Kurtis.Velarde@fox.com>
2017-07-22 17:21:13 -0700

    Adding consule groovy script

---
+++ jenkins/src/consul.groovy
@@ -0,0 +1,21 @@
+consul_endpoint='http://34.212.90.136:8500'
+
+def getConsulValue(def key){
+    return
+}
+
+def putConsulValue(def key){
+    url = "${consul_endpoint}/v1/kv/${key}"
+
+    def http = url.openConnection()
+    http.setDoOutput(true)
+    http.setRequestMethod('PUT')
+    http.setRequestProperty('User-Agent', 'groovy script')
+    def out = new OutputStreamWriter(http.outputStream)
+    out.write('Consul RUles!!')
+    out.close()
+    http.inputStream
+    return
+}
+
+return this
